056f6d9eae5faa8a8b0747c4d01ecf926a1acf84
Cody Olsen <stipsan@gmail.com>
2021-03-13 01:22:01 +0100

    lint and all that stuff

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../stories/*.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+    })
+
+    return config
+  },
+}
