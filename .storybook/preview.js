91cc2d4a722524d0034c41bb6416b8e5df797780
Cody Olsen <stipsan@gmail.com>
2021-03-13 02:42:37 +0100

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,16 @@
 import { enableMapSet } from 'immer'
+import { Toaster } from 'react-hot-toast'
 
 import 'tailwindcss/tailwind.css'
 import '../style.css'
 
 enableMapSet()
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Story />
+      <Toaster position="top-right" />
+    </>
+  ),
+]
